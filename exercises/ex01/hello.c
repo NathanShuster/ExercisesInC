3aef725615810b1fba2116b7c19068cd9b75e685
Nathan Shuster <nshuster8@gmail.com>
2019-01-30 17:17:53 -0500

    finshed hello

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+    printf("%d", y);
     return 0;
 }
+
+/*
+
+1. Adding int x = 5 adds the following in the Assembly code.
+
+subq	$16, %rsp
+movl	$5, -4(%rbp)
+
+It looks like it is allocating 16 bits then allocating the value 5 to it.
+
+2. After adding optimization, the above lines appear to disappear from the
+assembly code.
+
+3. Making it print x adds a lot of movl calls.
+Optimize appears to reduce the space allocated for x from 16 to 8 bits.
+
+4. The optimized version has significantly fewer movl calls, and from reading the
+output I suspect that in the process of optimation the compiler got rid of the x
+variable and just made y equal to 1 + 5.
+*/
